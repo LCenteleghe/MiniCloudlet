633a4d958b1700732ae48029fd4d310df7bc3008
Luis Gustavo Simioni Centeleghe <lg.simioni@outlook.com>
2017-04-14 19:45:12 -0300

    Adiciona cloudlet API.

---
+++ CloudletAPI/src/br/unisinos/edu/lcloudlet/api/SimpleQueryRequest.java
@@ -0,0 +1,22 @@
+package br.unisinos.edu.lcloudlet.api;
+
+import java.io.Serializable;
+
+public class SimpleQueryRequest implements Serializable {
+	private static final long serialVersionUID = 3575576140491136233L;
+	
+	private String query;
+	
+	public SimpleQueryRequest(String query){
+		this.query = query;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	@Override
+	public String toString() {
+		return "SimpleQueryRequest [query=" + query + "]";
+	}
+}
